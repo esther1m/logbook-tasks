e5e3afb785f92ff30379e9f2ff86756bb9fcdeb7
esther1m <146996363+esther1m@users.noreply.github.com>
2025-06-22 12:29:50 +0100

    objectss

--- index.js
+++ index.js
@@ -2,3 +2,17 @@ console.log('Hello World.');
 
 let name = 'esther';
 console.log(name);
+
+let interest_rate = 0.3;
+interest_rate = 1;
+console.log(interest_rate);
+
+const price = 2;
+console.log(price);
+
+let age = 21;
+
+let person = {
+    name_object: 'jess' ,
+    age_object: 19
+};
