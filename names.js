9379972cde4cb1d888f7a726fe63ecabba301b5d
esther1m <146996363+esther1m@users.noreply.github.com>
2025-07-08 22:32:20 +0100

    array of names written to page

--- names.js
+++ names.js
@@ -1,1 +1,9 @@
-prompt("What's your name? ");       
+let name1 = prompt("What's your name? ");       
+let name2 = prompt("What's your name? ");       
+let name3 = prompt("What's your name? ");       
+
+let names = [name1, name2, name3];
+
+document.write("Hello " + names[0] + " there are " + names[0].length + " letters in your name");
+document.write("Hello " + names[1] + " there are " + names[1].length + " letters in your name");
+document.write("Hello " + names[2] + " there are " + names[2].length + " letters in your name");
