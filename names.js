8389f3886a1116523bad2d8bce1662794c979265
esther1m <146996363+esther1m@users.noreply.github.com>
2025-07-12 20:22:52 +0100

    printed alert with names sorted alphabetically

--- names.js
+++ names.js
@@ -7,3 +7,7 @@ let names = [name1, name2, name3];
 document.write("Hello " + names[0] + " there are " + names[0].length + " letters in your name <br>");
 document.write("Hello " + names[1] + " there are " + names[1].length + " letters in your name <br>");
 document.write("Hello " + names[2] + " there are " + names[2].length + " letters in your name <br>");
+
+let alphabeticalNames = names.sort();
+
+console.log(alert("Here's a list of names in alphabetical order: " + alphabeticalNames));
