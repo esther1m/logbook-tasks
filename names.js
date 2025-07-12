3b8408d01423d37ecc649341fe9e135ba8673284
esther1m <146996363+esther1m@users.noreply.github.com>
2025-07-12 20:11:34 +0100

    added line breaks

--- names.js
+++ names.js
@@ -4,6 +4,6 @@ let name3 = prompt("What's your name? ");
 
 let names = [name1, name2, name3];
 
-document.write("Hello " + names[0] + " there are " + names[0].length + " letters in your name");
-document.write("Hello " + names[1] + " there are " + names[1].length + " letters in your name");
-document.write("Hello " + names[2] + " there are " + names[2].length + " letters in your name");
+document.write("Hello " + names[0] + " there are " + names[0].length + " letters in your name <br>");
+document.write("Hello " + names[1] + " there are " + names[1].length + " letters in your name <br>");
+document.write("Hello " + names[2] + " there are " + names[2].length + " letters in your name <br>");
